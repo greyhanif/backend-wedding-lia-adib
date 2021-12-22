8577de97e96cd06bb7e584450de9b4d945ba4eb7
greyhanif <grey.hanif@gmail.com>
2021-12-22 16:23:51 +0700

    edit env

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("wis_db", "root", "", {
+const db = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER_NAME}`, `${process.env.DB_PASSWORD}`, {
   host: "localhost",
   dialect: "mysql",
   timezone: "+07:00",
